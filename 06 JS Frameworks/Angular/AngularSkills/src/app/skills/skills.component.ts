c4f6f003927795190e74cefb69613cf16a4b0f05
Georg Abenthung <georg@abenthung.name>
2019-08-28 16:09:16 +0200

    Move skills service into skills folder.

--- 06 JS Frameworks/Angular/AngularSkills/src/app/skills/skills.component.ts
+++ 06 JS Frameworks/Angular/AngularSkills/src/app/skills/skills.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { SkillsService } from "../skills.service";
-import { Skill } from "../model";
+import { Component, OnInit } from '@angular/core';
+import { SkillsService } from './skills.service';
+import { Skill } from '../model';
 
 @Component({
-  selector: "app-skills",
-  templateUrl: "./skills.component.html",
-  styleUrls: ["./skills.component.scss"]
+	selector: 'app-skills',
+	templateUrl: './skills.component.html',
+	styleUrls: [ './skills.component.scss' ]
 })
 export class SkillsComponent implements OnInit {
-  skills: Skill[];
-  skillToAdd: string;
+	skills: Skill[];
+	skillToAdd: string;
 
-  constructor(private service: SkillsService) {}
+	constructor(private service: SkillsService) {}
 
-  ngOnInit() {
-    this.service.getSkills().subscribe(data => (this.skills = data));
-  }
+	ngOnInit() {
+		this.service.getSkills().subscribe((data) => (this.skills = data));
+	}
 
-  removeSkill(s: Skill) {
-    this.skills = this.skills.filter((i: Skill) => i !== s);
-  }
+	removeSkill(s: Skill) {
+		this.skills = this.skills.filter((i: Skill) => i !== s);
+	}
 
-  addSkill() {
-    let sk: Skill = { id: this.skills.length + 1, name: this.skillToAdd };
-    this.skills.push(sk);
-  }
+	addSkill() {
+		let sk: Skill = { id: this.skills.length + 1, name: this.skillToAdd };
+		this.skills.push(sk);
+	}
 }
