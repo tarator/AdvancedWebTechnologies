c39329446ac5ad6721bbf1c76a14a96bce88e536
Georg Abenthung <georg@abenthung.name>
2019-08-30 11:47:15 +0200

    Changes

--- 08 Real Time - PWA/ngSkillsPWA/src/environments/environment.prod.ts
+++ 08 Real Time - PWA/ngSkillsPWA/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: true,
-  apiUrl: "https://pwaapi.azurewebsites.net/api/",
-  firebase: {
-    apiKey: "AIzaSyDKOBVweF7puu4P60AiQUFTaspqlVXvFTM",
-    authDomain: "skills-5fb49.firebaseapp.com",
-    databaseURL: "https://skills-5fb49.firebaseio.com",
-    projectId: "skills-5fb49",
-    storageBucket: "skills-5fb49.appspot.com",
-    messagingSenderId: "1005178568143",
-    appId: "1:1005178568143:web:506dadae903b5a79"
-  }
+	production: true,
+	apiUrl: 'https://localhost:5000/api/',
+	firebase: {
+		apiKey: 'AIzaSyDKOBVweF7puu4P60AiQUFTaspqlVXvFTM',
+		authDomain: 'skills-5fb49.firebaseapp.com',
+		databaseURL: 'https://skills-5fb49.firebaseio.com',
+		projectId: 'skills-5fb49',
+		storageBucket: 'skills-5fb49.appspot.com',
+		messagingSenderId: '1005178568143',
+		appId: '1:1005178568143:web:506dadae903b5a79'
+	}
 };
--- 10 SecuritySocial/SecureSkills/src/environments/environment.ts
+++ 10 SecuritySocial/SecureSkills/src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: false,
-  token: "QeOv74uiEDNz7QM91bjYKc82VgS2"
+	production: false,
+	token: 'QeOv74uiEDNz7QM91bjYKc82VgS2',
+	firebase: {
+		apiKey: 'AIzaSyBl_VAJQonZpldb94jLUYBNAEMHKj9SuO4',
+		authDomain: 'skills-ad72d.firebaseapp.com',
+		databaseURL: 'https://skills-ad72d.firebaseio.com',
+		projectId: 'skills-ad72d',
+		storageBucket: '',
+		messagingSenderId: '78202231164',
+		appId: '1:78202231164:web:17f658a29564ff75'
+	}
 };
